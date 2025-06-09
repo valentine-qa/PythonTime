c5260c87f8eb7a1bc730ce956fe7b9ae9124e984
valentine-qa <valentine.borodich13@gmail.com>
2025-06-09 12:08:17 +0300

    Add a demoqa example

--- Lesson1/test_demoqa.py
+++ Lesson1/test_demoqa.py
@@ -1,7 +1,7 @@
 import time
 
 import pytest
-from selene import browser
+from selene import browser, by, have, be
 
 
 @pytest.fixture(scope="function")
@@ -23,4 +23,21 @@ def test_demoqa(browser_params):
     browser.element('#lastName').type('borodich')
     browser.element('#userEmail').type('valentineqa@gmail.com')
     browser.element('#currentAddress').type('Minsk, Belarus')
-    browser.element('#userNumber').type('447721735')
+    browser.element('#userNumber').type('4477217351')
+    browser.element('#genterWrapper').element(by.text("Male")).click()
+    browser.element("#dateOfBirthInput").click()
+    browser.element(".react-datepicker__month-select").element("[value='4']").click()
+    browser.element(".react-datepicker__year-select").element("[value='2000']").click()
+    # browser.element(".react-datepicker__day:not(.react-datepicker__day--outside-month)").element(by.text("13")).click()
+    browser.all(".react-datepicker__day:not(.react-datepicker__day--outside-month)").element_by(have.text('13')).click()
+    # browser.element('#subjectsWrapper').type('M').press_enter()
+    browser.element("#hobbiesWrapper").element("label[for=hobbies-checkbox-1]").click()
+    browser.element('#state').click()
+    browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+    browser.element('#city').click()
+    browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+    browser.element("#submit").click()
+
+    assert browser.element(".modal-content").should(be.visible)
+    assert browser.element(".modal-content").should(have.text("Thanks for submitting the form"))
+    assert browser.all(".table-responsive").element_by(have.exact_texts("Student Name", "valentine"))
