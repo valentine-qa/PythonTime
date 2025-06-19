567fc6e027c86f3668e031c2bab7c92d0ac28cd0
valentine-qa <valentine.borodich13@gmail.com>
2025-06-19 10:16:39 +0300

    Added demoqa test

---
+++ Lesson11_Jankins/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
